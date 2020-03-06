be77a381b680eabe0799bbf8d1566b636d3fc938
Surya D <Surya.dheeshjith@gmail.com>
2020-03-06 20:55:18 +0530

    Added ann_sigmoid- Binary classification

---
+++ Code/ann_sigmoid.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+from sklearn.utils import shuffle
+from util import getBinaryData,sigmoid,sigmoid_cost,relu,error_rate,init_weight_and_bias,tanh
+
+class ANN:
+    def __init__(self,M):
+        self.M = M
+
+    def fit(self,X,Y,learning_rate=5*10e-7,regularisation=1,epochs=10000,show_fig=False):
+        X,Y = shuffle(X,Y)
+        Y = np.reshape(Y,(len(Y),1))
+        # print("X.shape"+str(X.shape))
+        # print("Y.shape"+str(Y.shape))
+        Xvalid, Yvalid = X[-1000:],Y[-1000:]
+        X,Y = X[:-1000],Y[:-1000]
+        # print("X.shape"+str(X.shape))
+        # print("Y.shape"+str(Y.shape))
+        N,D = X.shape
+        self.W1,self.b1 = init_weight_and_bias(D,self.M)
+        self.W2,self.b2 = init_weight_and_bias(self.M,1) ###
+
+        costs = []
+        best_validation_error = 1
+        for i in range(epochs):
+            # forward propagation
+            pY, Z = self.forward(X)
+
+            # gradient descent
+            pY_Y = pY - Y
+            # print("X.shape"+str(X.shape))
+            # print("pY.shape"+str(pY.shape))
+            # print("Y.shape"+str(Y.shape))
+            # print("Z.shape"+str(Z.shape))
+            # print("W2.shape"+str(self.W2.shape))
+            # print("pY_Y.shape"+str(pY_Y.shape))
+            self.W2 -= learning_rate*(Z.T.dot(pY_Y) + regularisation*self.W2)
+            self.b2 -= learning_rate*(pY_Y.sum() + regularisation*self.b2)
+            dZ = pY_Y.dot(self.W2.T) * (Z>0)
+
+            self.W1 -= learning_rate*(X.T.dot(dZ) + regularisation*self.W1)
+            self.b1 -= learning_rate*(np.sum(dZ,axis=0) + regularisation*self.b1)
+
+            if i%20 ==0 :
+                pYvalid ,_ = self.forward(Xvalid)
+                # print("Yvalid.shape"+str(Yvalid.shape))
+                # print("pYvalid.shape"+str(pYvalid.shape))
+                c = sigmoid_cost(Yvalid,pYvalid)
+                costs.append(c)
+                e = error_rate(Yvalid, np.round(pYvalid))
+                print("i : "+str(i)+"; Cost : "+str(c)+"; Error : "+str(e))
+                if e < best_validation_error:
+                    best_validation_error = e
+
+        print("Best Validation error : "+str(best_validation_error))
+
+        if(show_fig):
+            plt.plot(costs)
+            plt.show()
+
+
+    def forward(self,X):
+        #Z = relu(X.dot(self.W1)+self.b1)
+        Z = tanh(X.dot(self.W1)+self.b1)
+        # print("Z.shape"+str(Z.shape))
+        # print("self.W2.shape"+str(self.W2.shape))
+        # print("self.b2.shape"+str(self.b2.shape))
+        ret =  sigmoid(Z.dot(self.W2)+self.b2)
+        # print("ret.shape"+str(np.array(ret).shape))
+        return ret, Z
+
+    def predict(self,X):
+        pY,_ = self.forward(X)
+        return np.round(pY)
+
+    def score(self,X,Y):
+        prediction = self.forward(X)
+        return (1 - error_rate(Y,prediction))*100
+
+
+
+
+
+
+def main():
+    X, Y = getBinaryData()
+    model = ANN(100)
+    model.fit(X,Y,show_fig = True)
+
+
+if __name__ == '__main__':
+    main()
--- Code/util.py
+++ Code/util.py
@@ -4,13 +4,16 @@ import pandas as pd
 def init_weight_and_bias(M1,M2):
     W = np.random.randn(M1,M2)/np.sqrt(M1+M2)
     b = np.zeros(M2)
-    return W.astype(np.float32), b.astype(np.float32)
-    
+    return W, b
+
 def relu(x):
     return x * (x>0)
-    
+
 def sigmoid(A):
     return 1/(1+np.exp(-A))
+
+def tanh(A):
+    return np.tanh(A)
     
 def softmax(A):
     expA = np.exp(A)
@@ -18,17 +21,17 @@ def softmax(A):
 
 def sigmoid_cost(T,Y):
     return -(T*np.log(Y)+(1-T)*np.log(1-Y)).sum()
-    
+
 def cost(T,Y):
     return -(T*np.log(Y)).sum()
-    
+
 def cost2(T,Y):###
     N = len(T)
     return -np.log(Y[np.arange(N),T]).sum() #Without zeros ; Indexing Y using T
-    
-def error_rate(targets,prediction):
+
+def error_rate(targets,predictions):
     return np.mean(targets!=predictions)
-    
+
 def y2indicator(y):
     N = len(y)
     K = len(set(y))
@@ -38,69 +41,61 @@ def y2indicator(y):
     return ind
 
 def getData(balance_ones=True):
-    
+
     path = '../Data/face_data.csv'
     data = pd.read_csv(path)
     y = data['emotion']
     X = []
     for i in range(len(data)):
         X.append(data[' pixels'][i].split(' '))
-    
-    
+
+
     X = np.array(X)
-    X = X.astype('float32')
+    X = X.astype('float64')
     X = np.divide(X,255.0)
     y = np.array(y)
-    
-    
+
+
     if(balance_ones):
         X0, y0 = X[y!=1], y[y!=1]
         X1 = X[y==1]
-        print("len(X0): "+str(len(X0)))
-        print("len(X1): "+str(len(X1)))
-        print("len(X0)/len(X1): "+str(len(X0)/len(X1)))
         X1 = np.repeat(X1,len(X0)/len(X1),axis=0)
         X = np.vstack([X0,X1])
         y = np.concatenate((y0,[1]*len(X1)))
-    
+
     return X,y
-    
+
 def getBinaryData(balance_ones=True):
     path = '../Data/face_data.csv'
     data = pd.read_csv(path)
-    
+
     y = []
     X = []
-    
+
     for i in range(len(data)):
         if(data.loc[i,'emotion']==0 or data.loc[i,'emotion']==1):
             y.append(data.loc[i,'emotion'])
             X.append(data[' pixels'][i].split(' '))
     X = np.array(X)
-    X = X.astype('float32')
+    X = X.astype('float64')
     X = np.divide(X,255.0)
     y = np.array(y)
-    
+
     if(balance_ones):
         X0, y0 = X[y==0], y[y==0]
         X1 = X[y==1]
-        print("len(X0): "+str(len(X0)))
-        print("len(X1): "+str(len(X1)))
-        print("len(X0)/len(X1): "+str(len(X0)/len(X1)))
         X1 = np.repeat(X1,len(X0)/len(X1),axis=0)
         X = np.vstack([X0,X1])
         y = np.concatenate((y0,[1]*len(X1)))
     return X,y
-    
-##TESTING
-#def main():
-#    X,y= getBinaryData()
-#    print(X.shape)
-#    print(y.shape)
-#
-#
-#
-#if __name__ == '__main__':
-#    main()
-#
-#
+
+#TESTING
+def main():
+   X,y= getBinaryData()
+   print(X.shape)
+   print(y.shape)
+
+
+
+if __name__ == '__main__':
+   main()
